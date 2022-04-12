1949b977daafc0b8724525f23791ba6e45a348ce
James Coffman <jamesc4023@gmail.com>
2022-04-12 16:44:11 -0400

    commit tree 6c9d98e07b5a6c46c9e3da1fd7b57d7fc81ba3af

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Add your code here
+const submitData = (userName, address) => {
+    return fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+        body: JSON.stringify({
+          name: userName,
+          email: address,
+        }),
+    })
+    .then (res => res.json())
+    .then (data => appendData(data.id))
+    .catch (error => appendData(error.message))
+}
+
+submitData()
+
+function appendData(stuff) {
+    const body = document.querySelector('body');
+    console.log(body)
+    const p = document.createElement('p');
+    p.textContent = stuff;
+    body.append(p);
+}
